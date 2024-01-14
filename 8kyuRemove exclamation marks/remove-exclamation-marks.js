2b50c1526e2bdda359d8886d896c5fdc4b1a82ab
ekaterinapupeka <etymanskaya@bk.ru>
2024-01-14 12:46:57 +0300

    remove-exclamation-marks.js

---
+++ 8kyuRemove exclamation marks/remove-exclamation-marks.js
@@ -0,0 +1,9 @@
+//DESCRIPTION:
+
+//Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+
+
+//SOLUTION:
+function removeExclamationMarks(s) {
+    return s.replace(/!/gi, '');
+}
