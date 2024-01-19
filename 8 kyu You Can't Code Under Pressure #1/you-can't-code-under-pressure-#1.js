99d703aeb3e02ce57ce16a448371135c39212119
ekaterinapupeka <etymanskaya@bk.ru>
2024-01-19 12:49:50 +0300

    you-can't-code-under-pressure-#1.js

---
+++ 8 kyu You Can't Code Under Pressure #1/you-can't-code-under-pressure-#1.js
@@ -0,0 +1,10 @@
+// DESCRIPTION:
+//
+//     Code as fast as you can! You need to double the integer and return it.
+//
+//     SOLUTION:
+
+function doubleInteger(i) {
+    // i will be an integer. Double it and return it.
+    return i * 2;
+}
