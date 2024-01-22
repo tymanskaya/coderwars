c703fc3deae0b4ab6e6d28eb026a9f74f414a9cd
ekaterinapupeka <etymanskaya@bk.ru>
2024-01-22 14:50:33 +0300

    switch-it-up!.js

---
+++ 8 kyu Switch it Up!/switch-it-up!.js
@@ -0,0 +1,15 @@
+// DESCRIPTION:
+//
+//     When provided with a number between 0-9, return it in words.
+//
+// Input :: 1
+//
+// Output :: "One".
+//
+// If your language supports it, try using a switch statement.
+//
+//     SOLUTION
+
+function switchItUp(number){
+    return ["Zero","One","Two","Three","Four","Five","Six","Seven","Eight","Nine"][number]
+}
