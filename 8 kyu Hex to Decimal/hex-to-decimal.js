a9d34fbe9f9e56db9edc5c46945780fb9ac708df
ekaterinapupeka <etymanskaya@bk.ru>
2024-01-25 09:11:56 +0300

    hex-to-decimal.js

---
+++ 8 kyu Hex to Decimal/hex-to-decimal.js
@@ -0,0 +1,11 @@
+// DESCRIPTION:
+//
+//     Complete the function which converts hex number (given as a string) to a decimal number.
+//
+// SOLUTION:
+
+// Long Solution
+    const hexToDec = hexString => parseInt(hexString, 16)
+
+// Function Export
+module.exports = hexToDec
