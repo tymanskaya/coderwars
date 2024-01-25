784004e0314ba2a381aebba07c440e6c044415f9
ekaterinapupeka <etymanskaya@bk.ru>
2024-01-25 08:54:46 +0300

    kata-example-twist.js

---
+++ 8 kyu Kata Example Twist/kata-example-twist.js
@@ -0,0 +1,10 @@
+// DESCRIPTION:
+//
+//     This is an easy twist to the example kata (provided by Codewars when learning how to create your own kata).
+//
+// Add the value "codewars" to the array websites 1,000 times.
+//
+//     SOLUTION:
+
+var websites = [];
+while (websites.length < 1000) websites.push("codewars")
