ac878df73ea15a5af50e9d5d925489fc45fbf839
ekaterinapupeka <etymanskaya@bk.ru>
2024-03-27 00:04:51 +0300

    no-oddities-here.js

---
+++ 7 kyu No oddities here/no-oddities-here.js
@@ -0,0 +1,7 @@
+// DESCRIPTION:
+// Write a small function that returns the values of an array that are not odd.
+//
+//     All values in the array will be integers. Return the good values in the order they are given.
+//     SOLUTION:
+
+var noOdds = values => values.filter(x => x % 2 === 0);
